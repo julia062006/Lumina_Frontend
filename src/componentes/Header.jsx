51ecc5a040430ca9230f4d0dec0530d4ed92f708
julia062006 <julia.rubim06@gmail.com>
2026-03-02 22:44:41 -0300

    Estilizando o header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,12 +1,20 @@
 import "./Header.css";
+import logo from "../imagens/LogoCelularLivro.png";
 
 export default function Header() {
     return (
         <header className="header">
             <div className="logo">
-                <img src=""></img>
-                <h1>Lumina</h1>
+                <img className="logoCelularLivro" src={logo} alt="Logo Lumina"></img>
+                <h1 className="logoTitulo">LUMINA</h1>
             </div>
+
+            <nav className="menu">
+                <a href="#inicio">Inicío</a>
+                <a href="#biblioteca">Biblioteca</a>
+                <a href="#categorias">Categorias</a>
+                <a href="#autores">Autores</a>
+            </nav>
         </header>
     );
 }
