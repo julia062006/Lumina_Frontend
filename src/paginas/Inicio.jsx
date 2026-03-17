78a38497ae0795b058d32a32b7bf3145a14cbd0c
julia062006 <julia.rubim06@gmail.com>
2026-03-16 22:45:01 -0300

    Componente card adicionado

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom";
 import { BotaoPrimario, BotaoSecundario } from "../componentes/Botao";
+import Card from "../componentes/Card";
+
 function Inicio() {
+
+    const bookTeste = {
+        image: "https://picsum.photos/300/400",
+        title: "Livro Teste",
+        author: "Autor Teste",
+        description: "Esse é apenas um livro para testar o card.",
+        price: 29.90
+    };
+
+    const handleAddToCart = (book) => {
+        console.log("Adicionado:", book);
+    };
+
     return (
         <div className="p-10">
             <h1 className="text-3xl font-bold">Ilumine sua Leitura.</h1>
+
             <p className="mt-4 text-gray-600">
-                Descubra uma nova dimensão na leitura digital. Acesso ilimitado aos melhores livros, em qualquer dispositivo, a qualquer momento.
+                Descubra uma nova dimensão na leitura digital. Acesso ilimitado aos melhores livros em qualquer lugar.
             </p>
 
             <div className="flex gap-4 mt-6">
@@ -14,6 +30,14 @@ function Inicio() {
                 <Link to="/cadastroUsuario">
                     <BotaoSecundario>Criar Conta</BotaoSecundario>
                 </Link>
+            </div>
+
+            {/* TESTE DO CARD */}
+            <div className="mt-10 w-64">
+                <Card
+                    book={bookTeste}
+                    onAddToCart={handleAddToCart}
+                />
             </div>
 
         </div>
