5c4c38a40deaeccb20ff6e19434e908bd67c9944
julia062006 <julia.rubim06@gmail.com>
2026-03-19 22:49:59 -0300

    Arrumando imagem na página início

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import { BotaoPrimario, BotaoSecundario } from "../componentes/Botao";
 import Card from "../componentes/Card";
-import bg from "../imagens/FundoInicio.png";
+import bg from "../imagens/Livros2.png";
+
 
 function Inicio() {
 
@@ -18,34 +19,41 @@ function Inicio() {
     };
 
     return (
-        <div className="p-20 min-h-screen bg-no-repeat bg-righ "
-            style={{
-                backgroundImage: `url(${bg})`, backgroundSize: "1000px", backgroundPosition: "right 35px" // 👈 controla o tamanho
-            }}>
-            <h1 className="text-3xl font-bold mt-20">Ilumine sua experiência na leitura</h1>
+        <main className="">
+            <div className="p-10 min-h-scren bg-no-repeat bg-righ "
+                style={{
+                    backgroundImage: `url(${bg})`, backgroundSize: "1000px", backgroundPosition: "right 35px"
+                    
+                     // 👈 aumenta aqui
+                  
+                }}>
+                <h1 className="text-5xl lg:text-6xl font-medium leading-tight tracking-tight">
+                    Ilumine sua <br />
+                    <span style={{ color: 'var(--lumina-purple)' }}>Leitura.</span>
+                </h1>
 
-            <p className="mt-4 text-gray-800 " >
-                Mais do que uma plataforma, a Lumina é o seu portal para conhecimento, imaginação e descoberta.
-            </p>
-            <p className="text-gray-800 "> 
-                Acesse milhares de livros, leia no seu ritmo e em qualquer lugar. 
-            </p> 
-            <p className=" text-gray-800 ">
-                Descubra novas histórias, aprenda e se inspire todos os dias.
-            </p>
-            
-            
-
-            <div className="flex gap-4 mt-6">
-                <BotaoPrimario>Explorar Livros</BotaoPrimario>
-
-                <Link to="/cadastroUsuario">
-                    <BotaoSecundario>Criar Conta</BotaoSecundario>
-                </Link>
-            </div>
+                <p className="text-lg text-muted-foreground max-w-lg gap-2 mt-4">
+                    Mais do que uma plataforma, a Lumina é o seu portal para conhecimento, imaginação e descoberta.
+                </p>
+                <p className="text-lg text-muted-foreground max-w-lg">
+                    Acesse milhares de livros, leia no seu ritmo e em qualquer lugar.
+                </p>
+                <p className="text-lg text-muted-foreground max-w-lg">
+                    Descubra novas histórias, aprenda e se inspire todos os dias.
+                </p>
 
 
-            {/* TESTE DO CARD 
+
+                <div className="flex gap-4 mt-6">
+                    <BotaoPrimario>Explorar Livros</BotaoPrimario>
+
+                    <Link to="/cadastroUsuario">
+                        <BotaoSecundario>Criar Conta</BotaoSecundario>
+                    </Link>
+                </div>
+
+
+                {/* TESTE DO CARD 
             <div className="mt-10 w-64">
                 <Card
                     book={bookTeste}
@@ -53,7 +61,8 @@ function Inicio() {
                 />
             </div> */}
 
-        </div>
+            </div>
+        </main>
     );
 }
 
